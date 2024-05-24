1a2eaed354b835af6f49278b574a0e47706ee124
Dorota Zabielska <Dorota@192.168.1.18>
2024-05-24 17:22:15 +0200

    creating first test home_main_elements_navigation

---
+++ tests/home_main_elements_navigation.js
@@ -0,0 +1,17 @@
+const { test, expect } = require('@playwright/test');
+const { baseUrl } = require('../data/config');
+const HomePage = require('../pages/homePage');
+
+test('navigate to home page', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await homePage.navigate();
+    await expect(page).toHaveURL(baseUrl);
+});
+
+test('click on contact us link and verify new page', async ({ page }) => {
+    const homePage = new HomePage(page);
+    await homePage.navigate();
+    await homePage.clickContactUs();
+    await expect(page).toHaveURL(`${baseUrl}Contact-Us/contactus.html`);
+    // Add additional assertions as needed
+});
